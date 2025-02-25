d25d28875be7b50be0032fc6df4c1f3c7f8552e5
Phuykong <m.phuykong@gmail.com>
2025-02-25 10:46:15 -0500

    reverse deployment

---
+++ MedMemo/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/phuykong/Desktop/Github/Documentation/MedMemo/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/phuykong/Desktop/Github/Documentation/MedMemo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/phuykong/Desktop/Github/Documentation/MedMemo/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/phuykong/Desktop/Github/Documentation/MedMemo/src/css/custom.css"),
+];
